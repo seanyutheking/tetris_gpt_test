97336692b4a8ad1781b7f4dd3e25eabdef588ca0
seanyutheking <sean.yu@velymonkeys.com>
2025-10-16 17:27:41 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Tetris from "./Tetris";
+
+export default function App() {
+  return (
+    <div className="min-h-screen flex justify-center items-center bg-gray-50">
+      <Tetris />
+    </div>
+  );
+}
---
+++ Tetris.jsx
@@ -0,0 +1,4 @@
+// 🎮 여기에 앞서 만든 완성형 테트리스 코드 전체를 붙여넣으세요.
+export default function Tetris() {
+  return <div>🎮 Tetris Game Placeholder</div>;
+}
